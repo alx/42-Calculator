6dca123491171a356d2a84a1158b56ba697cb73b
Alexandre Girard <git@alexgirard.com>
2011-10-01 11:38:10 +0200

    add media

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,30 @@
 
 */
 
+$(function() {
+  var LedWall = Backbone.Model.extend({
+    //updateBOM: function(){
+    //  console.log("updateBOM");
+    //},
+    log: function(){
+      console.log("ledwall: " + this.get("height") + "x" + this.get("width"));
+    }
+  });
 
+  window.ledwall = new LedWall({
+    height: parseInt($("#resizable").css("height"), 10),
+    width: parseInt($("#resizable").css("width"), 10)
+  });
 
+  $("#resizable").resizable({
+    containment: "#ledwall"
+  });
 
+  $( "#resizable" ).bind( "resize", function(event, ui) {
+    ledwall.set({height: parseInt($("#resizable").css("height"), 10)});
+    ledwall.set({width: parseInt($("#resizable").css("width"), 10)});
+    ledwall.log();
+  });
+
+});
 
